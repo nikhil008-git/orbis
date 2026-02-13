b911ab620e954a5f43cbaaa0b9b55d55ba86bd72
nikhil <gittthub008@gmail.com>
2026-02-13 20:05:28 +0000

    build logs check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import FooterSection from '@/components/common/footer'
 import { Hero } from '@/components/home/hero'
 import FAQsTwo from "@/components/home/faq"
+import { AnimatedGroup } from "@/components/ui/animated-group"
+import { transitionVariants } from "@/lib/animation-variants"
 export default function Home() {
   return (
     <>
       <Hero />
       <FAQsTwo />
-      <FooterSection />
+      <AnimatedGroup variants={transitionVariants}>
+        <FooterSection />
+      </AnimatedGroup>
     </>
   )
 }
